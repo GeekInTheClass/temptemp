b694225547e56be6798dc47f2f887112a50bcc03
cadenzah <cadenzah93@gmail.com>
2019-04-11 17:50:28 +0900

    Modified Problem 4

--- Problem4.playground/Contents.swift
+++ Problem4.playground/Contents.swift
@@ -1,21 +1,15 @@
-// Problem 04. Sort: 코드 유효성 검사
+// Problem 04. filter: 추리기
 
-// 고양이가 찢어버린 코드를 다 모으고 보니, 정모는 자신의 모은 코드 조각들이 전부 멀쩡한 상태인지 의심이 들었다.
-// 그래서 코드 조각들을 하나하나 살펴보며 검사해보기로 하였다.
-// 그런데 정모는 순서에 굉장히 민감하여서, 검사에 앞서서 각 조각들에 적힌 내용을 기준으로 조각들을 순서대로 나열해보기로 하였다.
+// 배열에서 짝수만 골라내세요
 
-// Object: 코드 조각들을 순서대로 나열하세요.
-// 단, 정렬 작업을 위한 함수를 직접 작성해야 합니다.
-// 코드 조각들은 String으로 이루어진 배열 변수로 주어집니다.
-// 각 조각들은 띄어쓰기로 이루어져 있습니다.
+// 예시 입력: let arr: Array<Int> = [1, 2, 3, 4, 5, 6, 7, 8]
+// 예시 출력: [2, 4, 6, 8]
 
-// 예시 입력: let arr: Array<String> = ["#include", "'stdio.h'", "int", "main(void)", "{", "return", "0;", "}"]
-// 예시 출력: ["#include", "\'stdio.h\'", "0;", "int", "main(void)", "return", "{", "}"]
+let arr: Array<Int> = [1, 2, 3, 4, 5, 6, 7, 8]
+var result: Array<Int> = []
 
-var pieces: Array<String> = ["#include", "'stdio.h'", "int", "main(void)", "{", "return", "0;", "}"]
+for i in arr {
+    if (i % 2 == 0) { result += [i] }
+}
 
-print(pieces)
-
-pieces.sort()
-
-print(pieces)
+print(result)
